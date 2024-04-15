326fce5990e1a860d8f4209d0893944d61fddc1e
Mehrdad <52593172+mehrdad987@users.noreply.github.com>
2024-04-15 16:23:38 +0200

    main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 #include "stdio.h"
 
-
+//Monitoring in Serial
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 
 I2C_HandleTypeDef hi2c1;
@@ -28,13 +28,8 @@ int bcdToDec(uint8_t val)
 }
 
 typedef struct {
-	uint8_t seconds;
-	uint8_t minutes;
-	uint8_t hour;
-	uint8_t dayofweek;
-	uint8_t dayofmonth;
-	uint8_t month;
-	uint8_t year;
+	uint8_t seconds;uint8_t minutes;uint8_t hour;
+	uint8_t dayofmonth;uint8_t month;uint8_t year;
 } TIME;
 
 TIME time;
@@ -43,26 +38,21 @@ TIME time;
 
 void Set_Time (uint8_t sec, uint8_t min, uint8_t hour, uint8_t dow, uint8_t dom, uint8_t month, uint8_t year)
 {
-	uint8_t set_time[7];
-	set_time[0] = decToBcd(sec);
-	set_time[1] = decToBcd(min);
-	set_time[2] = decToBcd(hour);
-	set_time[3] = decToBcd(dow);
-	set_time[4] = decToBcd(dom);
-	set_time[5] = decToBcd(month);
-	set_time[6] = decToBcd(year);
+	uint8_t set_time[6];
+	set_time[0] = decToBcd(sec);set_time[1] = decToBcd(min);set_time[2] = decToBcd(hour);
 
-	HAL_I2C_Mem_Write(&hi2c1, DS3231_ADDRESS, 0x00, 1, set_time, 7, 1000);
+	set_time[4] = decToBcd(dom);set_time[5] = decToBcd(month);set_time[6] = decToBcd(year);
+
+	HAL_I2C_Mem_Write(&hi2c1, DS3231_ADDRESS, 0x00, 1, set_time, 6, 1000);
 }
 
 void Get_Time (void)
 {
-	uint8_t get_time[7];
-	HAL_I2C_Mem_Read(&hi2c1, DS3231_ADDRESS, 0x00, 1, get_time, 7, 1000);
+	uint8_t get_time[6];
+	HAL_I2C_Mem_Read(&hi2c1, DS3231_ADDRESS, 0x00, 1, get_time, 6, 1000);
 	time.seconds = bcdToDec(get_time[0]);
 	time.minutes = bcdToDec(get_time[1]);
 	time.hour = bcdToDec(get_time[2]);
-	time.dayofweek = bcdToDec(get_time[3]);
 	time.dayofmonth = bcdToDec(get_time[4]);
 	time.month = bcdToDec(get_time[5]);
 	time.year = bcdToDec(get_time[6]);
